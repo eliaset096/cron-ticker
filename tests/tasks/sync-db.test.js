4fcd9b289c398cbea71dfc47044a870b2383a35d
Elías Estupiñan <Elias.Estupinan@carvajal.com>
2023-10-08 00:22:23 -0500

    second commit

--- tests/tasks/sync-db.test.js
+++ tests/tasks/sync-db.test.js
@@ -1,7 +1,7 @@
 const syncDB = require("../../tasks/sync-db");
 
 describe('Pruebas en Sync-DB', () => {
-    test('Debe de sincronizar la base de datos 2 veces', async () => {
+    test('Debe de sincronizar la base de datos multiplo de 5, dos veces', async () => {
         // 1. Preparación
         // 2. Estímulo
         // 3. Observar el comportamiento
@@ -10,7 +10,7 @@ describe('Pruebas en Sync-DB', () => {
         const times = syncDB();
         console.log('Se llamó sync-db -->', times);
 
-        expect(times).toBe(2);
+        expect(times).toBe(10);
 
     });
 });
